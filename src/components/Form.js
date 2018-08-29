fc3374d959b647b59fc7323388545ee5ef909599
surabhi maheshwari <surabhimaheshwari.cs@gmail.com>
2018-08-28 20:43:33 -0400

    First Commit

---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import React from 'react';
+class Form extends React.Component{
+  render(){
+    return(
+<form onSubmit={this.props.getWeather}>
+<input type="text" name="city" placeholder="City"/>
+<input type="text" name="country" palceholder="Country"/>
+<button > Get weather </button>
+</form>
+    );
+  }
+};
+export default Form;
